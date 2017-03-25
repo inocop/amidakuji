5a0d2a43b6bc7f495b4fff97e31cdbdd7e113099
inocop <inoue19851110@gmail.com>
2017-03-25 15:45:53 +0900

    shellを修正。

--- bin/docker-run-test.sh
+++ bin/docker-run-test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd `dirname $0`
-cd ..
+cd `dirname $0`/..
+dir=`pwd`
+docker_dir="/var/amidakuji/"
 
-docker run --rm -it -v $(pwd):/var/amidakuji php:7.0 php /var/amidakuji/vendor/bin/phpunit /var/amidakuji/tests
+docker run --rm -it -v ${dir}:${docker_dir} php:7.0 \
+phpdbg -qrr ${docker_dir}vendor/bin/phpunit \
+--coverage-html ${docker_dir}docs/coverage \
+--white ${docker_dir}src ${docker_dir}tests
